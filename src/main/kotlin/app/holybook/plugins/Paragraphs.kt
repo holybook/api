ce43487265d7996b7f7060a7592590bbf0a13293
Hannes Widmoser <widmoser@gmail.com>
2022-12-11 22:41:02 +0100

    Fix paragraph detection and add inserting paragraphs into db

--- src/main/kotlin/app/holybook/plugins/Paragraphs.kt
+++ src/main/kotlin/app/holybook/plugins/Paragraphs.kt
@@ -1,5 +1,7 @@
 package app.holybook.plugins
 
+import app.holybook.models.Books
+import app.holybook.models.Paragraphs
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
@@ -11,34 +13,71 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
 val client = HttpClient()
 
 fun Routing.configureParagraphs() {
     post("books/{id}/paragraphs") {
         val content = call.receive<BookContent>()
-        val response = client.get(content.sourceUrl)
-        val contentType = response.contentType()
+        val paragraphContent = client.get(content.sourceUrl)
+        val contentType = paragraphContent.contentType()
 
-        if (contentType?.match("application/pdf") == true) {
-            // Handle PDF.
-            val paragraphs = parsePdf(response.body())
-            System.err.println(paragraphs)
-            call.respond(HttpStatusCode.OK)
-            return@post
+        val skipStart = call.request.queryParameters["skipStart"]?.toInt() ?: 0
+        val skipEnd = call.request.queryParameters["skipStart"]?.toInt() ?: 0
+
+        val paragraphs =
+            getParagraphs(contentType, paragraphContent.body())
+        if (paragraphs != null) {
+            val filteredParagraphs = paragraphs.subList(
+                fromIndex = skipStart,
+                toIndex = paragraphs.size - skipEnd
+            )
+            transaction {
+                filteredParagraphs.forEachIndexed { i, paragraph ->
+                    Paragraphs.insert {
+                        it[bookId] = call.parameters["id"]?.toInt()!!
+                        it[index] = i
+                        it[language] = content.language
+                        it[text] = paragraph
+                    }
+                }
+            }
+            call.respond(
+                HttpStatusCode.OK,
+                AddParagraphsResponse(paragraphsAdded = filteredParagraphs.size)
+            )
+        } else {
+            call.respond(HttpStatusCode.NotImplemented)
         }
-
-        call.respond(HttpStatusCode.NotImplemented)
     }
 }
 
-fun parsePdf(content: ByteArray): Iterable<String> {
+fun getParagraphs(
+    contentType: ContentType?,
+    content: ByteArray
+): List<String>? {
+    if (contentType?.match("application/pdf") == true) {
+        // Handle PDF.
+        return parsePdf(content)
+    }
+
+    return null
+}
+
+fun parsePdf(content: ByteArray): List<String> {
     val pdfDocument = PDDocument.load(content)
     val pdfTextStripper = PDFTextStripper()
+    pdfTextStripper.paragraphStart = "/t";
+    pdfTextStripper.sortByPosition = true;
     val text = pdfTextStripper.getText(pdfDocument)
 
-    return text.split('\n')
+    return text.split(pdfTextStripper.paragraphStart)
 }
 
 @Serializable
-data class BookContent(val sourceUrl: String)
+data class BookContent(val sourceUrl: String, val language: String)
+
+@Serializable
+data class AddParagraphsResponse(val paragraphsAdded: Int)
