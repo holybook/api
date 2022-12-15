86c0dcecd1c13b67720bf9fbcb4b2a2ceb4a6df6
Hannes Widmoser <widmoser@gmail.com>
2022-12-15 01:31:36 +0100

    Add paragraph merging
    to improve automatic paragraph splitting

--- src/main/kotlin/app/holybook/plugins/PdfParser.kt
+++ src/main/kotlin/app/holybook/plugins/PdfParser.kt
@@ -19,10 +19,30 @@ class PdfParser : ParagraphParser {
                 it.value.replace(pdfTextStripper.paragraphStart, "")
             }
 
-        return text.split(pdfTextStripper.paragraphStart).map {
-            it.trim()
-        }.filter {
-            it.isNotEmpty()
+        val paragraphs = text.split(pdfTextStripper.paragraphStart).map { it.trim() }.filter { it.isNotEmpty() }
+        val mergedParagraphs = mutableListOf<String>()
+        var pendingParagraph = ""
+        for (p in paragraphs) {
+            if (p.isFullParagraph()) {
+                mergedParagraphs.add(pendingParagraph + p)
+                pendingParagraph = ""
+            } else {
+                pendingParagraph += p
+            }
         }
+
+        return mergedParagraphs
+    }
+
+    private fun String.endsWithStop(): Boolean {
+        return endsWith('.') || endsWith('!') || endsWith('?')
+    }
+
+    private fun String.isFullParagraph(): Boolean {
+        return endsWithStop() || isTitle()
+    }
+
+    private fun String.isTitle(): Boolean {
+        return length < 256 && get(0).isUpperCase()
     }
 }
