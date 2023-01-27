e8d7f8dde4ba64171076917f910fbaf852ab71ea
Hannes Widmoser <widmoser@google.com>
2023-01-27 14:20:20 +0100

    Removed unused Index class

--- import/src/main/kotlin/app/holybook/import/Index.kt
+++ import/src/main/kotlin/app/holybook/import/Index.kt
@@ -6,36 +6,28 @@ import app.holybook.api.db.Database.transaction
 import app.holybook.api.models.createBooksTable
 import app.holybook.api.models.createParagraphsTable
 import app.holybook.api.models.createTranslationsTable
-import io.ktor.util.logging.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
-import org.slf4j.LoggerFactory
 
 suspend fun fetchAndImportIndex() {
-    val index: List<BookInfo> = Json.decodeFromStream(
-        Json::class.java.getResourceAsStream("/index.json")
-    )
+  val index: List<BookInfo> =
+    Json.decodeFromStream(Json::class.java.getResourceAsStream("/index.json"))
 
-    transaction {
-        // Create tables:
-        createBooksTable()
-        createTranslationsTable()
-        createParagraphsTable()
-    }
+  transaction {
+    // Create tables:
+    createBooksTable()
+    createTranslationsTable()
+    createParagraphsTable()
+  }
 
-    index.forEach {
-        fetchAndImportBook(it)
-    }
+  index.forEach { fetchAndImportBook(it) }
 }
 
 @Serializable
-data class Index(val bookInfos: List<BookInfo>)
-
-@Serializable
 data class BookInfo(
-    val id: String,
-    val original: ContentInfo,
-    val translations: List<ContentInfo> = listOf()
+  val id: String,
+  val original: ContentInfo,
+  val translations: List<ContentInfo> = listOf(),
 )
