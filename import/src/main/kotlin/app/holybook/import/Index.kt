ca77e7d0e3046c6c335c23a051d0b47379daf6c3
Hannes Widmoser <widmoser@google.com>
2023-02-01 16:08:01 +0100

    Remove unused imports

--- import/src/main/kotlin/app/holybook/import/Index.kt
+++ import/src/main/kotlin/app/holybook/import/Index.kt
@@ -1,14 +1,7 @@
-@file:OptIn(ExperimentalSerializationApi::class)
-
 package app.holybook.import
 
 import app.holybook.import.model.ContentDescriptor
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromStream
 
 suspend fun fetchAndImportIndex(descriptors: Flow<ContentDescriptor>) {
   descriptors.collect { fetchAndImport(it) }
