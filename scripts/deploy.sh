b593bd2ee38c5d5922975cdebaff8bbe8e4bbc66
Hannes Widmoser <widmoser@gmail.com>
2023-03-18 14:52:17 +0300

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,14 +8,16 @@ cp server/build/libs/server-all.jar dist/build
 cp import/build/libs/import-all.jar dist/build
 
 mkdir dist/client
-(cd webclient || npm start)
+(cd webclient && npm run build)
 cp -r webclient/build/* dist/client/
-
-mkdir -p /var/server/data
 
 zip build/dist.zip -r dist
 scp build/dist.zip ubuntu@holybook.app:~/
 
+ssh ubuntu@holybook.app 'sudo mkdir -p /var/server/data'
 ssh ubuntu@holybook.app 'sudo rm -rf dist'
 ssh ubuntu@holybook.app 'unzip dist.zip'
 ssh ubuntu@holybook.app 'rm dist.zip'
+
+ssh ubuntu@holybook.app '(cd dist && sudo docker compose down)'
+ssh ubuntu@holybook.app '(cd dist && sudo docker compose up --build -d)'
