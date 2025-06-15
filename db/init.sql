00c64f6b067ad0c83e45454e294ec409c6556157
Hannes Widmoser <widmoser@gmail.com>
2025-06-15 21:11:57 +0200

    Grant database and schema-level permissions to `server` user

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,10 @@
 CREATE USER server;
 
-DROP TABLE paragraphs;
-DROP TABLE translations;
-DROP TABLE books;
+-- grant access to the database
+GRANT CONNECT ON DATABASE holybook TO server;
 
+-- let the role create objects in the public schema
+GRANT USAGE, CREATE ON SCHEMA public TO server;
+
+-- keep the existing table-level privileges
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO server;
