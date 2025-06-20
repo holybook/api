b60e1d2dd2150e3e22a45f575722a383cd0d7e9e
Hannes Widmoser <widmoser@gmail.com>
2025-06-21 00:26:16 +0200

    Finish a first version of the prompt.

--- lib/src/main/kotlin/app/holybook/lib/translation/Translation.kt
+++ lib/src/main/kotlin/app/holybook/lib/translation/Translation.kt
@@ -1,6 +1,7 @@
 package app.holybook.lib.translation
 
 import app.holybook.lib.models.TranslateRequest
+import app.holybook.lib.models.TranslateResponse
 import app.holybook.lib.models.translate
 import com.google.genai.Client
 import org.slf4j.LoggerFactory
@@ -10,27 +11,41 @@ object Translation {
   private val log = LoggerFactory.getLogger("translation")
   val client = Client()
 
+  val TranslateResponse.id: String
+    get() = "${allOriginalResults[0].bookId}:${translatedParagraph.index}"
+
   fun translate(
     fromLanguage: String,
     toLanguage: String,
     text: String
   ): String {
-    val paragraphs = text.lines()
+    val paragraphs =
+      text.lines().mapNotNull { it.trim().takeIf { it.isNotBlank() } }
 
-    val translateResponses =
-      paragraphs.mapNotNull { paragraphText ->
-        translate(
+    val translationPairs =
+      paragraphs.map { paragraphText ->
+        val translateResponse = translate(
           TranslateRequest(
             fromLanguage = fromLanguage,
             toLanguage = toLanguage,
             text = paragraphText,
           )
         )
+        TranslationPair(
+          authoritativeTranslation = translateResponse,
+          textToBeTranslated = ParagraphToBeTranslated(
+            translateResponse?.id,
+            paragraphText
+          ),
+        )
       }
 
     val prompt = TranslationTemplateEngine.renderPrompt(
       TranslationTemplateData(
-        translateResponses
+        fromLanguage,
+        toLanguage,
+        translationPairs.mapNotNull { it.authoritativeTranslation },
+        translationPairs.map { it.textToBeTranslated }
       )
     )
 
@@ -44,3 +59,8 @@ object Translation {
 
   }
 }
+
+data class TranslationPair(
+  val authoritativeTranslation: TranslateResponse?,
+  val textToBeTranslated: ParagraphToBeTranslated
+)
--- lib/src/main/kotlin/app/holybook/lib/translation/TranslationTemplateData.kt
+++ lib/src/main/kotlin/app/holybook/lib/translation/TranslationTemplateData.kt
@@ -2,4 +2,14 @@ package app.holybook.lib.translation
 
 import app.holybook.lib.models.TranslateResponse
 
-data class TranslationTemplateData(val translateResponses: List<TranslateResponse>)
+data class ParagraphToBeTranslated(
+  val translationId: String?,
+  val text: String
+)
+
+data class TranslationTemplateData(
+  val fromLanguage: String,
+  val toLanguage: String,
+  val authoritativeTranslations: List<TranslateResponse>,
+  val paragraphsToBeTranslated: List<ParagraphToBeTranslated>
+)
